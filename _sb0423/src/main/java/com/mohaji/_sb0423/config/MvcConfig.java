0ed0853256682ae8bec535e67715812545fd3908
jjuiru <shj00035@naver.com>
2024-04-25 17:31:50 +0900

    0425_23Check

--- _sb0423/src/main/java/com/mohaji/_sb0423/config/MvcConfig.java
+++ _sb0423/src/main/java/com/mohaji/_sb0423/config/MvcConfig.java
@@ -1,13 +1,22 @@
 package com.mohaji._sb0423.config;
 
+import com.mohaji._sb0423.interceptor.LoggerInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
-    @Override
+    @Override        //컨트롤러를 사용하지 않고 첫 화면시 루트로 가는 명령어 실행
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("main");
+    }
+
+    @Override // 만들어진 인터셉터를 등록한다. (특정 폴더에만 인터셉트 적용)
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoggerInterceptor())
+                .addPathPatterns("/register/**")
+        .excludePathPatterns("/practice/**");
     }
 }
---
+++ _sb0423/src/main/java/com/mohaji/_sb0423/interceptor/LoggerInterceptor.java
@@ -0,0 +1,22 @@
+package com.mohaji._sb0423.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoggerInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("====preHandle====");
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        System.out.println("====postHandle====");
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+}
